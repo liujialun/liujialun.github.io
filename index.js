4089997171c74623e13d4e0040948bbcd63b6452
6 Gallon <jialun.liu@outlook.com>
2019-01-21 14:11:30 +0800

    add flight attitude indicator

---
+++ index.js
@@ -0,0 +1,14 @@
+'use strict';
+
+Object.defineProperty(exports, "__esModule", {
+	value: true
+});
+exports.AttitudeIndicator = undefined;
+
+var _AttitudeIndicator = require('./AttitudeIndicator/AttitudeIndicator');
+
+var _AttitudeIndicator2 = _interopRequireDefault(_AttitudeIndicator);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+exports.AttitudeIndicator = _AttitudeIndicator2.default;
